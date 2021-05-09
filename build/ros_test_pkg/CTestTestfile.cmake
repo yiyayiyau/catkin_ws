20fdd32c5cf6eb844fd44975bb2fe152191d3a58
heng <hengundwang@gmail.com>
2021-05-09 18:29:43 +0200

    delete build and devel

--- build/ros_test_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/heng/catkin_ws/src/ros_test_pkg
-# Build directory: /home/heng/catkin_ws/build/ros_test_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
