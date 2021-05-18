1d37c46bc00a82de189e6c2cc985403e26f61510
heng <hengundwang@gmail.com>
2021-05-18 11:22:39 +0200

    add parameter_config c++ py, rosparam-command

---
+++ src/learning_parameter/scripts/parameter_config.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#import sys
+import rospy
+from std_srvs.srv import Empty
+
+def parameter_config():
+    
+    rospy.init_node('parameter_config', anonymous=True)
+
+    red = rospy.get_param('/turtlesim/background_r')
+    green = rospy.get_param('/turtlesim/background_g')
+    blue = rospy.get_param("/turtlesim/background_b")
+
+    rospy.loginfo("Get Background Color[%d %d %d]", red, green, blue)
+
+    rospy.set_param("/turtlesim/background_r", 255)
+    rospy.set_param("/turtlesim/background_g", 255)
+    rospy.set_param('/turtlesim/background_b', 255)
+
+    rospy.loginfo("Re-Get Background Color[%d %d %d]", red, green, blue)
+
+    rospy.wait_for_service('/clear')
+    
+    try:
+        clear_background = rospy.ServiceProxy('/clear', Empty)
+        response = clear_background()
+        return response
+    except rospy.ServiceException as err:
+        print("Service call failed: %s"%err)
+
+if __name__=="__main__":
+    try:
+        parameter_config()
+    except rospy.ROSInterruptException:
+        pass
